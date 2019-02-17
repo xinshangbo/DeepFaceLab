8c2831dba12c2b2d0b08b770a42e27b240e8ffc6
xinshangbo <xinshangbo@zhiyinet.cn>
2019-02-17 12:01:15 +0800

    add shell script

--- clear.sh
+++ clear.sh
@@ -1,5 +1,5 @@
-rm -f ./workspace/data_dst/*
-rm -f ./workspace/data_dst/align/*
-rm -f ./workspace/data_src/*
-rm -f ./workspace/data_src/align/*
+rm -f -r ./workspace/data_dst/*
+mkdir ./workspace/data_dst/align
+rm -f -r ./workspace/data_src/*
+mkdir ./workspace/data_src/align
 rm -f ./workspace/models/*
---
+++ extract.sh
@@ -0,0 +1,2 @@
+ffmpeg -i ./workspace/data_src.mp4 -y ./workspace/data_src/data_src_%03d.png
+ffmpeg -i ./workspace/data_dst.mp4 -y ./workspace/data_dst/data_dst_%03d.png
